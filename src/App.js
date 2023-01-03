7f9ff33446825e444b0f4667cf40c4b14d85906e
GUchechi <godswilluchendu7@gmail.com>
2023-01-03 14:38:21 +0100

    added Login2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/Login";
+import Login2 from "./components/Login2";
 
 function App() {
   return (
     <div>
       <Login />
+      <Login2 />
     </div>
   );
 }
